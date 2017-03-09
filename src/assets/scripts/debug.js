f39fc30144fc575b9f74507fc6254f4b63bf2c2c
Pieter Beekman <pieter.beekman@incentro.com>
2017-03-09 11:43:39 +0100

    Added some extra console log buttons

--- src/assets/scripts/debug.js
+++ src/assets/scripts/debug.js
@@ -1,6 +1,53 @@
 // Console log
+document.getElementById('log').onclick = function() {
+    consoleLog();
+};
 
-console.log('disco');
+function consoleLog() {
+    console.log('Een normale melding in de console');
+}
+
+
+// Console log array
+document.getElementById('logArray').onclick = function() {
+    logArray();
+};
+
+function logArray() {
+    let musicArray = ['ABBA', 'REM', 'Foo Fighters', 'Editors'];
+    console.log(musicArray);
+}
+
+
+// Console log object
+document.getElementById('logObject').onclick = function() {
+    logObject();
+};
+
+function logObject() {
+    let firstMusicObject = {song:'ABBA - Dancing Queen', id:11};
+    console.log(musicObject)
+}
+
+// Console log error
+document.getElementById('error').onclick = function() {
+    consoleLogError();
+};
+
+function consoleLogError() {
+    console.error('O no! This is an error!');
+}
+
+// Console log info
+document.getElementById('info').onclick = function() {
+    consoleLogInfo();
+};
+
+function consoleLogInfo() {
+    console.info('This is an info message. Nothing to see here!');
+}
+
+
 
 
 // Console info
